65a57041c75a809f747725a239b6782a2100e181
ericbraz <89194596+ericbraz@users.noreply.github.com>
2022-12-08 17:26:21 -0300

    creation of loader component

--- components/OnePager/OnePager.tsx
+++ components/OnePager/OnePager.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import Footer from '../Footer'
+import Layout from '../reusables/layouts/Layout'
 import TopMenu from '../TopMenu'
 import WebsiteContent from '../WebsiteContent'
 
 function OnePager() {
    return (
-      <>
+      <Layout>
          <TopMenu />
          <WebsiteContent />
          <Footer />
-      </>
+      </Layout>
    )
 }
 
