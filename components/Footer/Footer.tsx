680a8acc9c38a845db1c4bb315177d7e0042fb00
ericbraz <89194596+ericbraz@users.noreply.github.com>
2023-03-17 15:48:00 -0300

    language setting applied to footer component

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import styles from './Footer.module.scss'
-import { BsInstagram, BsTwitter, BsLinkedin } from 'react-icons/bs'
+import { BsInstagram, BsTwitter, BsLinkedin, BsGithub } from 'react-icons/bs'
 import MyCircleComponent from './MyCircleComponent'
+import { useSelector } from 'react-redux'
+import { useLanguages } from '../../redux/features/language/languageSlice'
 
 function Footer() {
-   const margins = 'm-0'
+   const margins = 'm-0 mx-6 text-center'
    const compSizes = 25
+
+   const language = useSelector(useLanguages)
+   const footerLangs = [
+      { lang: 'en-us', text: ['Info • ', 'Terms of Use • ', 'Privacy Policy'] },
+      { lang: 'pt-br', text: ['Info • ', 'Termos de Uso • ', 'Política de Privacidade'] },
+      {
+         lang: 'fr-fr',
+         text: ['Info • ', "Conditions d'Utilisation • ", 'Politique de Confidentialité'],
+      },
+   ]
 
    /* The spans below are meant to become link in the future */
    return (
@@ -14,11 +26,15 @@ function Footer() {
             <MyCircleComponent Icon={BsInstagram} size={compSizes} />
             <MyCircleComponent Icon={BsTwitter} size={compSizes} />
             <MyCircleComponent Icon={BsLinkedin} size={compSizes} />
+            <MyCircleComponent Icon={BsGithub} size={compSizes} />
          </div>
          <div className={`${margins}`}>
-            <span className='hover:cursor-pointer'>Info</span>&nbsp;•&nbsp;
-            <span className='hover:cursor-pointer'>Termos de Uso</span>&nbsp;•&nbsp;
-            <span className='hover:cursor-pointer'>Política de Privacidade</span>
+            {footerLangs.map(
+               (footlang) =>
+                  footlang.lang === language && (
+                     <span className='hover:cursor-pointer'>{footlang.text}</span>
+                  )
+            )}
          </div>
          <div className={`text-gray-400 ${margins}`}>© ericlima.com.br</div>
       </div>
