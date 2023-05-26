c9b5d7025cf9cb629006dc9c09be0fd252f1b890
ericbraz <89194596+ericbraz@users.noreply.github.com>
2023-05-26 17:33:38 -0300

    Language set automatically

--- components/OnePager/OnePager.tsx
+++ components/OnePager/OnePager.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Footer from '../Footer'
 import Layout from '../reusables/layouts/Layout'
 import TopMenu from '../TopMenu'
 import WebsiteContent from '../WebsiteContent'
+import useLanguage from '../../hooks/useLanguage'
+import { Languages } from '../../interfaces/languagesType'
 
 function OnePager() {
+   const { setLanguage } = useLanguage()
+   useEffect(() => {
+      const userLanguage = navigator.language.split('-')[0];
+      if (['en', 'pt', 'fr'].includes(userLanguage)) setLanguage(userLanguage as Languages);
+   }, [])
+
    return (
       <Layout>
          <TopMenu />
