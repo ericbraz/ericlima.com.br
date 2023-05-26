f7d245ed7086bf0919249b9f0803b51475ffaf2e
ericbraz <89194596+ericbraz@users.noreply.github.com>
2023-05-26 14:53:13 -0300

    1st section set

--- components/reusables/verse/CTAButton/CTAButton.tsx
+++ components/reusables/verse/CTAButton/CTAButton.tsx
@@ -7,12 +7,14 @@ function CTAButton({
    color,
    style,
    className,
+   buttonClassName,
 }: {
    children: any
    bgColor?: string
    color?: string
    style?: React.CSSProperties
    className?: string
+   buttonClassName?: string
 }) {
    const finalBgColor = bgColor ? bgColor : '#2CF53D'
    const finalColor = color ? color : '#000000'
@@ -26,9 +28,12 @@ function CTAButton({
          backgroundColor: finalBgColor,
       }
 
+   const standardBtnClasses = `${styles.button} font-normal px-7 py-2 rounded-lg cursor-pointer no-underline`
+   const finalBtnClassName = buttonClassName ? standardBtnClasses + ' ' + buttonClassName : standardBtnClasses
+
    return (
       <div className={className}>
-         <a role='cta' className={`${styles.button} 'font-semibold px-7 py-2 rounded-lg cursor-pointer no-underline'`} style={finalCss}>
+         <a role='cta' className={finalBtnClassName} style={finalCss}>
             {children}
          </a>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-     extend: {
-        animation: {
-           fadeIn: 'fadeIn .15s ease-in'
-        },
-        keyframes: {
-           fadeIn: {
-              from: { opacity: 0 },
-              to: { opacity: 1 }
-           }
-        }
-     },
-  },
-  plugins: [],
+   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+   theme: {
+      extend: {
+         animation: {
+            fadeIn: 'fadeIn .15s ease-in',
+         },
+         keyframes: {
+            fadeIn: {
+               from: { opacity: 0 },
+               to: { opacity: 1 },
+            },
+         },
+         screens: {
+            360: '360px',
+            420: '420px',
+            550: '550px',
+            785: '785px',
+            870: '870px',
+         },
+      },
+   },
+   plugins: [],
 }
